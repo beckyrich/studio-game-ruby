6085d6980036eb89d3bb68e141c4ae8947341a9b
Becky Richardson <rebecca.s.richardson4@gmail.com>
2023-03-12 18:59:10 -0500

    Variables and Objects

--- studio_game.rb
+++ studio_game.rb
@@ -2,6 +2,28 @@ name1 = 'larry'
 health1 = 60
 
 name2 = 'curly'
-name3 = 'moe'
+health2 = 125
 
-puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
+name3 = 'moe'
+health3 = 100
+
+name4 = 'shemp'
+health4 = 90
+
+# puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
+puts "#{name1.capitalize} has a health of #{health1}"
+puts "#{name2.upcase} has a health of #{health2}"
+puts " #{name3.capitalize} has a health of #{health3}. ".center(40, '*')
+puts "#{name4.capitalize.ljust(30, '.')} #{health4} health."
+
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "The game started on #{formatted_time}"
+
+
+# health2 = health1
+# puts "#{name2.upcase} has a health of #{health2}"
+
+# health1 = 30
+# puts "#{name1.capitalize} has a health of #{health1}."
+# puts "#{name2.upcase} has a health of #{health2}."
