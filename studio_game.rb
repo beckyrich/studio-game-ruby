0b9fbd629864e6b2436389c52eeed179b4a8fb80
Becky Richardson <rebecca.s.richardson4@gmail.com>
2023-05-23 19:28:39 -0500

    Attributes

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,15 @@
 class Player
+    
+    attr_accessor :name
+    attr_reader :health
+
     def initialize(name, health=100)
         @name = name.capitalize
         @health = health
     end
 
     def to_s
-        "I'm #{@name} with a health of #{@health}."
+        "I'm #{@name} with a health of #{@health} and a score of #{score}."
     end
 
     def blam
@@ -17,12 +21,25 @@ class Player
         @health += 15
         puts "#{@name} got wOOted!"
     end
+
+    def score
+        @health + @name.length
+    end
 end
 
 player1 = Player.new("moe")
 puts player1
+puts player1.name
+puts player1.health
+puts player1.score
+puts player1
 
 player2 = Player.new("larry", 60)
+puts player2.name
+player2.name = "Lawrence"
+puts player2.name
+puts player2.health
+puts player2.score
 puts player2
 
 player3 = Player.new("curly", 125)
