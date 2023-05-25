2ee402e0c4025ebf457d348ad525292244b14f0e
Becky Richardson <rebecca.s.richardson4@gmail.com>
2023-05-25 13:19:28 -0500

    Arrays

--- studio_game.rb
+++ studio_game.rb
@@ -48,3 +48,26 @@ player3.blam
 puts player3
 player3.w00t
 puts player3
+
+players = [player1, player2, player3]
+puts "There are #{players.size} players in the game."
+
+players.each do |player|
+    puts player
+end
+
+players.each do |player|
+    puts player.health
+end
+
+players.each do |player|
+    player.blam
+    player.w00t
+    player.w00t
+    puts player
+end
+
+players.pop
+player4 = Player.new("Shemp", 90)
+players.push(player4)
+puts players
