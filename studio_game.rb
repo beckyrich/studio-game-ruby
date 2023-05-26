fbcd3b872233b9d20f1c4b5f1cf71c7c36cd457f
Becky Richardson <rebecca.s.richardson4@gmail.com>
2023-05-26 10:40:55 -0500

    Objects Interacting

--- studio_game.rb
+++ studio_game.rb
@@ -25,49 +25,43 @@ class Player
     def score
         @health + @name.length
     end
+    
+end
+
+class Game
+
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+        @players = []
+    end
+
+    def add_player(a_player)
+        @players << a_player
+    end
+
+    def play
+        puts "There are #{@players.size} players in #{@title}: "
+        @players.each do |player|
+            puts player
+        end
+        @players.each do |player|
+            player.blam
+            player.w00t
+            player.w00t
+            puts player
+        end
+    end
+
 end
 
 player1 = Player.new("moe")
-puts player1
-puts player1.name
-puts player1.health
-puts player1.score
-puts player1
-
 player2 = Player.new("larry", 60)
-puts player2.name
-player2.name = "Lawrence"
-puts player2.name
-puts player2.health
-puts player2.score
-puts player2
-
 player3 = Player.new("curly", 125)
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
 
-players = [player1, player2, player3]
-puts "There are #{players.size} players in the game."
-
-players.each do |player|
-    puts player
-end
-
-players.each do |player|
-    puts player.health
-end
-
-players.each do |player|
-    player.blam
-    player.w00t
-    player.w00t
-    puts player
-end
-
-players.pop
-player4 = Player.new("Shemp", 90)
-players.push(player4)
-puts players
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
